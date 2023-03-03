263fdc0582d5c18aa4efefe5b22d84248aab9038
abdullahi Hear <abdalaheart@gmail.com>
2023-03-03 04:13:29 -0800

    int & double

--- variable/variable.dart
+++ variable/variable.dart
@@ -1,11 +1,13 @@
 void main() {
   /*Variable
-  Basic Data type in Dart :
-  String , Booleams, Numbers
-  lists(Array), maps(json) 
+  //Basic Data type in Dart :
+  //String , Booleams, Numbers
+  //lists(Array), maps(json) 
   String greeting = 'Hello world';//String Example
   print(greeting);
   bool isNew = true;
-  print(isNew);*/
-  
+  print(isNew);
+  //int and double
+  double amount = 19.23;
+  print(amount);*/
 }
