2b9d022ddb3317657d9aefe6d72269140d46a55a
abdullahi Hear <abdalaheart@gmail.com>
2023-03-03 04:09:49 -0800

    variables

---
+++ variable/variable.dart
@@ -0,0 +1,11 @@
+void main() {
+  /*Variable
+  Basic Data type in Dart :
+  String , Booleams, Numbers
+  lists(Array), maps(json) 
+  String greeting = 'Hello world';//String Example
+  print(greeting);
+  bool isNew = true;
+  print(isNew);*/
+  
+}
